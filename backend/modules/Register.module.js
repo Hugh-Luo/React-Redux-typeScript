1bc835fb1ef242af1b4baafdfe9149a47c35a011
Hugh Luo <hugh.luo599@gmail.com>
2020-12-22 12:46:32 +1100

    Using register to connect mongodb

--- backend/modules/Register.module.js
+++ backend/modules/Register.module.js
@@ -4,8 +4,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema
 
 const UserSchema = new Schema ( {
-    email: String,
-    password: String
+    email: {
+        type:String,
+        required: true
+    },
+    password: {
+        type:String,
+        required:true
+    }
 })
 
 const User = mongoose.model('User', UserSchema);
--- backend/routes/Register.js
+++ backend/routes/Register.js
@@ -14,7 +14,67 @@ router.route('/register').post((req, res) => {
     const email = req.body.email
     const password = req.body.password
 
-    res.send({email, password})
+    const checkUser = () => {
+        return new Promise((resovle, reject)=>{
+            User.find()
+                .then(users=>{
+                    users.map(user=> {
+                        if (user.email === email) {
+                            // res.json('THe user already exist')
+                            reject('already exist')
+
+                        }
+                    })
+                    resovle('new User')
+
+                })
+                .catch(err => res.json(err))
+        })
+
+    }
+
+
+    const addUser = ()=> {
+        return new Promise((resolve, reject)=>{
+            // res.send({email, password})
+            const newUser = new User({
+                email,
+                password
+            })
+
+            newUser.save()
+                .then(response=>
+                    resolve('Add successfully' + '\n' + response))
+                .catch(err=>
+                    reject(err)
+                )
+        })
+    }
+
+
+    const register = () => {
+        checkUser()
+            .then(message => console.log(message))
+            .then(()=>addUser())
+            .then(mesage=>{
+                    console.log(mesage)
+                    res.json(mesage)
+                }
+            )
+            .catch(err=> {
+                console.log(err)
+                res.json(err)
+            })
+    }
+
+    register()
+
+})
+
+router.route('/users/delete').delete((req,res)=>{
+    User.deleteMany({})
+        .then(response=>{res.json({'All records have been deleted': response})})
+        .catch(err=>res.json(err))
 })
 
 module.exports = router;
